3cfcadd009c5662267d9dfbe0cd36cf5cd96607f
Adam McArthur <big-adz@hotmail.com>
2014-02-05 21:38:37 +1100

    Add option to add a html class to select tag
    Also cleaned up some of the code within the html_select method. Gave the select element an "id" equal to its "name" for easier javascript targeting. Enabled ability to set the resource_name as nil and just use a field name.

--- lib/html_helper.rb
+++ lib/html_helper.rb
@@ -1,17 +1,17 @@
 module HtmlHelpers
 
   # Basic implementation of a HTML SELECT helper
-  def html_select(resource_name, field_name, options_list, selected_value)
-    html = ''
-    options_list.each do |nv_pair|
-      option_value = nv_pair.keys.first
-      option_name = nv_pair.values.first
-      html << "<option value=\"#{option_value}\""
+  def html_select(resource_name, field_name, options_list, selected_value, html_class)
+    html = ""
+    options_list.each do |kv_pair|
+      option_value = kv_pair.keys.first
+      option_name = kv_pair.values.first
+      html << "<option value=\"#{option_value}\" id=\"#{option_value}\""
       html << " selected=\"true\"" if option_value == selected_value
-      html << '>'
+      html << ">"
       html << option_name
       html << "</option>"
     end
-    "<select name=\"[#{resource_name}]#{field_name}\">#{html}</select>"
+    "<select name=\"#{if resource_name}[#{resource_name}]#{end}#{field_name}\" id=\"[#{resource_name}]#{field_name}\"#{if html_class} class=\"#{html_class}\"#{end}>#{html}</select>"
   end
 end

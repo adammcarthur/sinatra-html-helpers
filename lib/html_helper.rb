0037c536ead8f8d24986abde26ece1728ecf9779
Adam McArthur <big-adz@hotmail.com>
2014-02-05 22:40:32 +1100

    Update html_helper.rb

--- lib/html_helper.rb
+++ lib/html_helper.rb
@@ -1,18 +1,16 @@
 module HtmlHelpers
 
   # HTML SELECT Helper
-  def html_select(resource_name, field_name, options_list, selected_value, html_class)
+  def html_select(resource_name, field_name, options_list, selected_value = nil, html_class = nil)
     html = ""
-    options_list.each do |kv_pair|
-      option_value = kv_pair.keys.first
-      option_name = kv_pair.values.first
+    options_list.each do |option_value, option_name|
       html << "<option value=\"#{option_value}\" id=\"#{option_value}\""
       html << " selected=\"true\"" if option_value == selected_value
       html << ">"
       html << option_name
       html << "</option>"
     end
-    "<select name=\"#{if resource_name}[#{resource_name}]#{end}#{field_name}\" id=\"#{if resource_name}#{resource_name}_#{end}#{field_name}\"#{if html_class} class=\"#{html_class}\"#{end}>#{html}</select>"
+    "<select name=\"[#{resource_name}]#{field_name}\" id=\"#{resource_name}_#{field_name}\" class=\"\">#{html}</select>"
   end
   
 end

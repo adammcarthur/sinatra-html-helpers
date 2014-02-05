056c7cfec87e3a2d955cac72b1a515f99fbaffe5
Adam McArthur <big-adz@hotmail.com>
2014-02-05 22:09:07 +1100

    Update html_helper.rb

--- lib/html_helper.rb
+++ lib/html_helper.rb
@@ -1,6 +1,6 @@
 module HtmlHelpers
 
-  # Basic implementation of a HTML SELECT helper
+  # HTML SELECT Helper
   def html_select(resource_name, field_name, options_list, selected_value, html_class)
     html = ""
     options_list.each do |kv_pair|
@@ -12,6 +12,7 @@ module HtmlHelpers
       html << option_name
       html << "</option>"
     end
-    "<select name=\"#{if resource_name}[#{resource_name}]#{end}#{field_name}\" id=\"#{if resource_name}[#{resource_name}]#{end}#{field_name}\"#{if html_class} class=\"#{html_class}\"#{end}>#{html}</select>"
+    "<select name=\"#{if resource_name}[#{resource_name}]#{end}#{field_name}\" id=\"#{if resource_name}#{resource_name}_#{end}#{field_name}\"#{if html_class} class=\"#{html_class}\"#{end}>#{html}</select>"
   end
+  
 end
